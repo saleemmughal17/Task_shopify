afc4bba4a160f1334eb183be6bf1abf4ad926507
saleemmughal17 <saleem177mughal@gmail.com>
2025-03-27 15:00:14 +0500

    Change

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+const AppConfig = {
+    site_name: "My Website",
+    title: "My Awesome App",
+    description: "Best app ever built!",
+    locale: "en",
+  };
+  
+  export default AppConfig;
+  
